fcb80df4ee953b99f181fba6ac5fbbf6fc1af03e
kcavs <kcav1321@gmail.com>
2024-04-15 14:31:53 -0400

    dones

---
+++ AccountSetup.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class AccountSetup{
+    public static void main (String[]args){
+        String user;
+        String pass;
+        int length;
+        Scanner input=new Scanner(System.in);
+        System.out.print("enter a username: ");
+        user=input.nextLine();
+        System.out.print("enter a password that is at least 8 characters: ");
+        pass=input.nextLine();
+        length= pass.length();
+        while(length<8){
+            System.out.print("enter a password that is at least 8 characters: ");
+            pass= input.nextLine();
+            length=pass.length();
+        }
+        System.out.print("your username is "+user+" and you password is "+pass);
+
+
+    }
+}
